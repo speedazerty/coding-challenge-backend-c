fb2e2dcec911814a8eb6f8436cc42edf6e33f6a4
Yannick Cordinier <yannick.cordinier@ssense.com>
2022-03-15 16:06:55 -0400

    Add request validation on latitude and longitude

--- src/controllers/validation/getSuggestionsSchema.ts
+++ src/controllers/validation/getSuggestionsSchema.ts
@@ -3,8 +3,17 @@ import Joi from 'joi';
 export class GetSuggestionsSchema {
     public static getSchema(): Joi.Schema {
         return Joi.object().keys({
-            q: Joi.string().required().min(1),
-            latitude: Joi.number().optional(),
+            q: Joi.string().required().min(1).message(''),
+            latitude: Joi.number()
+                .when('longitude', {
+                    is: Joi.number().exist(),
+                    then: Joi.required(),
+                    otherwise: Joi.forbidden(),
+                })
+                .messages({
+                    'any.unknown':
+                        'Latitude and Longitude must be provided together or none of them',
+                }),
             longitude: Joi.number().optional(),
         });
     }
